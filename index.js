68504443c8b755c628e1d86ee6eda5b9c3dff825
Kunal <dkunal125@gmail.com>
2024-01-23 11:47:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const calculateFormEl = document.getElementById("calculateForm")
+const resultEl = document.getElementById("result")
+
+const calculateMarks = (event) => {
+    const maxMarks = 400;
+    event.preventDefault();
+
+
+    const formData = new FormData(calculateFormEl);
+
+    const data = {};
+
+    formData.forEach((value,key)=>{
+        data[key]= +value;
+    });
+
+    const totalMarks = data.maths + data.science + data.hindi + data.english;
+    const percentage = Math.floor((totalMarks/maxMarks) * 100);
+    resultEl.innerText = `You Have Got ${totalMarks} marks out of ${maxMarks} and your percentage is ${percentage}%`;
+};
